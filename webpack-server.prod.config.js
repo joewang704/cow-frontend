09eccde3cedf28042347e1cdb9d89f9d513024e6
joewang704 <joewang704@gmail.com>
2016-11-10 16:39:42 -0500

    Its a port problem

--- webpack-server.prod.config.js
+++ webpack-server.prod.config.js
@@ -48,14 +48,6 @@ module.exports = {
       },
     ],
   },
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('production'),
-				'PORT': 80,
-			}
-		})
-  ],
 };
 
 
