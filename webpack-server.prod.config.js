b0f4490a3cb5d971acff040302c4f9f9299e5731
joewang704 <joewang704@gmail.com>
2016-11-10 16:27:09 -0500

    random shit

--- webpack-server.prod.config.js
+++ webpack-server.prod.config.js
@@ -51,7 +51,8 @@ module.exports = {
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
+				'NODE_ENV': JSON.stringify('production'),
+				'PORT': 80,
 			}
 		})
   ],
